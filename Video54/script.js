884a817d47b606fbe1dff71fefdcbcd380c3f411
atulsharma3982 <atulsharma8544@gmail.com>
2025-06-07 16:57:48 +0530

    JS intro

---
+++ Video54/script.js
@@ -0,0 +1,17 @@
+alert("Hello World")
+
+console.log("This is console.log")
+console.log("This is also console.log")
+var a = prompt("Enter a number");
+var b = confirm("Do u want to blast your computer?");
+if(b){
+    console.log("Computer is blasted")
+}
+else{
+    console.log("Computer is not blasted")
+}
+console.log("Entered number is " +a)
+
+document.title = "Hello World"
+
+document.body.style.backgroundColor = "black"
