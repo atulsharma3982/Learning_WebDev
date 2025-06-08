cf74735766468ef2a4dc252a0c05ddea7fd13419
atulsharma3982 <atulsharma8544@gmail.com>
2025-06-08 11:48:14 +0530

    JS Variables

---
+++ Video55/script.js
@@ -0,0 +1,36 @@
+console.log("This video 55");
+let a = 5;
+let b = 6;
+let c = "srtin";
+let _a = "hello";
+// console.log(a+b+8);
+// console.log(typeof a,typeof b,typeof c);
+
+{
+    let a = 10;
+    console.log(a);
+}
+
+console.log(a);
+
+let x = "ryoiki tenkai";
+let y = 25;
+let z = 36.63;
+let p = undefined;
+let q = null;
+
+console.log(x,y,z,p,q);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q);
+
+let o={
+    name: "Ryoiki Tenkai",
+    age: 25,
+    isActive: true,
+    "run code": 5050,
+}
+
+console.log(o);
+o.salary=5406576835;
+console.log(o);
+o.salary=9906576835;
+console.log(o);
