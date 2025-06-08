06984310861bf0fdb6efe6614b6c27c1d523e404
atulsharma3982 <atulsharma8544@gmail.com>
2025-06-08 14:35:39 +0530

    Conditionals

---
+++ Video56/index.js
@@ -0,0 +1,27 @@
+// console.log("Hello World");
+
+let age = 16;
+let grace = 2;
+age+=grace;
+// console.log(age);
+// console.log(age+grace);
+// console.log(age-grace);
+// console.log(age*grace);
+// console.log(age/grace);
+// console.log(age**grace);
+// console.log(age%grace);
+
+if(age>18){
+    console.log("You can drive");
+}
+else if(age==18){
+    console.log("You can learn to drive");
+}
+else{
+    console.log("You cannot drive");
+}
+
+let a=5;
+let b=8;
+let c = a>b?(a-b):(b-a);
+console.log(c);
