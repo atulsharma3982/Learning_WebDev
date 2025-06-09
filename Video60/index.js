c93ca03b1950409f60039bad920a377924965e7b
atulsharma3982 <atulsharma8544@gmail.com>
2025-06-09 17:57:14 +0530

    JS strings

---
+++ Video60/index.js
@@ -0,0 +1,22 @@
+console.log("This is a strings tutorial");
+let real_name = "Atul";
+let friend = "Ravi";
+// console.log(real_name[0]);
+// console.log(real_name[1]);
+// console.log(real_name[2]);
+// console.log(real_name[3]);
+// for(let i=0;i<real_name.length;i++){
+//     console.log(real_name[i]);
+// }
+console.log("My name is " + real_name + " and friend's name is " + friend);
+console.log(`My name is ${real_name} and friend's name is ${friend}`);
+let b="Dragonball";
+console.log(b.length);
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.slice(2,5));
+console.log(b.replace("Dr","dr$"));
+console.log(b.concat(real_name,"Dr","dr$"));
+let c="   hihn  sff ";
+// console.log(c);
+console.log(c.trim());
