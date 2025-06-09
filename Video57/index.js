904796cb323f9cf060bb5fbe2e67422bc414a81a
atulsharma3982 <atulsharma8544@gmail.com>
2025-06-09 11:08:45 +0530

    JS loops

---
+++ Video57/index.js
@@ -0,0 +1,31 @@
+console.log("I am a tutorial on loops");
+
+// let a = 1;
+// for (let i = 0; i < 10; i++) {
+//     console.log(a + i);
+// }
+// let obj = {
+//     name: "atul",
+//     role: "Programmer",
+//     title: "magicknight"
+// }
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key);
+// }
+// let b = "adhd";
+// for (const c of b) {
+//     console.log(c);
+// }
+
+// let i=0;
+// while(i<=600){
+//     console.log(i);
+//     i++;
+// }
+
+let i=10;
+do {
+    console.log(i);
+    i++;
+} while (i<6);
