0140f0467cdae007845b8851a658867ca98b35c0
atulsharma3982 <atulsharma8544@gmail.com>
2025-06-12 10:08:30 +0530

    DOM family

---
+++ Video67/script.js
@@ -0,0 +1,11 @@
+console.log("Hello World");
+document.body
+document.body.childNodes
+document.body.firstElementChild.childNodes
+document.body.childNodes[1].children[3].style.backgroundColor="Green"
+document.body.childNodes[1].children[3].nextSibling.textContent="This is a text node"
+document.body.childNodes[1].children[3].nextElementSibling
+document.body.childNodes[1].children[3].previousElementSibling
+document.body.childNodes[1].children[3].previousSibling
+document.body.childNodes[1].children[3].previousSibling.parentNode
+document.body.childNodes[1].children[3].previousSibling.parentElement
