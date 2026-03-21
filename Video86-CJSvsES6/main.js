3499633f26cacea382fed9f395c2159a757757b6
atulsharma3982 <atulsharma8544@gmail.com>
2026-03-21 11:24:46 +0530

    Common JS vs Ecmascript module

---
+++ Video86-CJSvsES6/main.js
@@ -0,0 +1,33 @@
+// // const { createServer } = require('node:http');
+// // const http = require('node:http');
+// // const fs = require("fs");
+
+// import http from "node:http"
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// // const server = createServer((req, res) => {
+// //     res.statusCode = 200;
+// //     res.setHeader('Content-Type', 'text/html');
+// //     res.end('<h1> Hello World </h1>');
+// // });
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.end('<h1> Hello World </h1>');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// import { a, b, c} from "./mymodule.js"
+// console.log(a, b, c); // named  need same name and  { }
+
+import name from "./mymodule.js"
+console.log(name); // default any name no need of { }
+
+// const a = require("./mymodule2.js")
+// console.log(a)
---
+++ Video86-CJSvsES6/mymodule.js
@@ -0,0 +1,14 @@
+export const a = 1; //named export
+export const b = 2;
+export const c = 5;
+export const d = 3;
+export const e = 4;
+
+
+
+let obj = {
+    x: 1,
+    y: 2
+}
+
+export default obj; // default export
