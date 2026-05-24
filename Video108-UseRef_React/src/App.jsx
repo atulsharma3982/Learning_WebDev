4ac836bde236139778c265b98905da552d892086
atulsharma3982 <atulsharma8544@gmail.com>
2026-05-24 20:26:14 +0530

    useRef in react

---
+++ Video108-UseRef_React/src/App.jsx
@@ -0,0 +1,153 @@
+import { useState,useEffect,useRef } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  /*
+  Use State maintains state and rerenders component to display the changed value
+  Because the value once displayed on the screen might be changed in the backend but it won't change on the screen until and unless the component is rerendered which we check using useEffect
+  case 1 where it works on every re reneder
+  Next we use a normal variable now we can't persist it's state when the component is rerendered because the code works again hence reinitializes it
+  So useRef is used it persists data across rerender
+  The changed data won't show on display until and unless it's rerendered but it will change and not reinitialize it will presist
+  Another usage of ref is that we don't need to use DOM to ref and element we can reference it using useRef
+  */
+ // const a = useRef(0)
+ // useEffect(() => {
+  //   a.current += 1;
+  //   console.log(`Rerendering...${a.current}`)
+  // })
+
+
+  const btnRef=useRef()
+  useEffect(() => {
+    console.log("First rendering")
+    btnRef.current.style.backgroundColor="red"
+    btnRef.current.style.color = "white"
+    // btnRef.current.innerText="anei"
+  })
+  let a = 0
+  
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          ref={btnRef}
+          type="button"
+          className="counter"
+          onClick={() => {
+            // setCount((count) => count + 1)
+            a+=1
+            btnRef.current.innerText = "Done" + a
+            //innerText is not recommended because you will have to maunally change data on ui but state rerenders whenever data is changed automatically   
+          }}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
